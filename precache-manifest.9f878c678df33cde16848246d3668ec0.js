60908525a1d5bf18e8ba8181cb5cd91f2bb069ee
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-19 08:29:26 +0000

    Deploy KotAleksei/js-band-final-task to github.com/KotAleksei/js-band-final-task.git:gh-pages

--- precache-manifest.9f878c678df33cde16848246d3668ec0.js
+++ precache-manifest.9f878c678df33cde16848246d3668ec0.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ca0f9cc3d60dd9ea6a7994b0bfe21deb",
+    "revision": "a8839ce5cdbb983844ce1ff8879e974a",
     "url": "/js-band-final-task/index.html"
   },
   {
-    "revision": "aca167130b977677b273",
+    "revision": "cabe4ea7f54d96f9145d",
     "url": "/js-band-final-task/static/css/2.64a99b10.chunk.css"
   },
   {
-    "revision": "81f5ac2b2d2c62959c8d",
+    "revision": "4debc03f006856a201cf",
     "url": "/js-band-final-task/static/css/main.cfb0a408.chunk.css"
   },
   {
-    "revision": "aca167130b977677b273",
-    "url": "/js-band-final-task/static/js/2.5323d16d.chunk.js"
+    "revision": "cabe4ea7f54d96f9145d",
+    "url": "/js-band-final-task/static/js/2.12afdda0.chunk.js"
   },
   {
     "revision": "84a2a7a390df1ac99fa4fadf23155e60",
-    "url": "/js-band-final-task/static/js/2.5323d16d.chunk.js.LICENSE"
+    "url": "/js-band-final-task/static/js/2.12afdda0.chunk.js.LICENSE"
   },
   {
-    "revision": "81f5ac2b2d2c62959c8d",
-    "url": "/js-band-final-task/static/js/main.d826f432.chunk.js"
+    "revision": "4debc03f006856a201cf",
+    "url": "/js-band-final-task/static/js/main.056c74a6.chunk.js"
   },
   {
     "revision": "f953aa2293d6504cd70a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/js-band-final-task/precache-manifest.703db67b6a09ad0a913cdbbb134fec2e.js"
+  "/js-band-final-task/precache-manifest.9f878c678df33cde16848246d3668ec0.js"
 );
 
 self.addEventListener('message', (event) => {
